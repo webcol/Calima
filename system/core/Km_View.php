588c2ccf5952369f5660c6f0d9b76222a9325586
Efrasoft <webcol@users.noreply.github.com>
2013-12-27 20:22:51 -0500

    Update Km_View.php

--- system/core/Km_View.php
+++ system/core/Km_View.php
@@ -1,1 +1,35 @@
-#
+<?php
+
+
+class Km_View
+{
+    private $_controlador;
+    
+    public function __construct(Km_Solicitud $peticion) {
+        $this->_controlador = $peticion->getControlador();
+    }
+    
+    public function renderizar($vista, $item = false)
+    {
+        
+        
+        $_layoutParams = array(
+            'ruta_css' => BASE_URL . 'mvc/views/layout/' . DEFAULT_LAYOUT . '/css/',
+            'ruta_img' => BASE_URL . 'mvc/views/layout/' . DEFAULT_LAYOUT . '/img/',
+            'ruta_js' => BASE_URL . 'mvc/views/layout/' . DEFAULT_LAYOUT . '/js/',
+            'menu' => $menu
+        );
+        
+        $rutaView = VIEW_PATH . $this->_controlador . DS . $vista . '.phtml';
+        
+        if(is_readable($rutaView)){
+            include_once ROOT . 'mvc/views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'header.php';
+            include_once $rutaView;
+            include_once ROOT . 'mvc/views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'menu.php';
+            include_once ROOT . 'mvc/views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'footer.php';
+        } 
+        else {
+            throw new Exception('Error de vista');
+        }
+    }
+}
