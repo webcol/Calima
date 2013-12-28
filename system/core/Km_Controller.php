b540cd6b29bed6744dbb1fcf347fabd54bf6169d
Efrasoft <webcol@users.noreply.github.com>
2013-12-27 20:20:25 -0500

    Update Km_Controller.php

--- system/core/Km_Controller.php
+++ system/core/Km_Controller.php
@@ -5,6 +5,6 @@ abstract class Km_Controller
     protected $_view;
     
     public function __construct() {
-        $this->_view = new Km_View(new Km_Request);
+        $this->_view = new Km_View(new Km_Solicitud);
     }
 }
