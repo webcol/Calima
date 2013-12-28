07ebce6e6d59ec3332372386d0d0b59bb07e8173
Efrasoft <webcol@users.noreply.github.com>
2013-12-27 20:18:09 -0500

    Update Km_Config.php

--- system/core/Km_Config.php
+++ system/core/Km_Config.php
@@ -1,1 +1,21 @@
-#
+<?php
+
+
+define('BASE_URL', 'localhost/kalima/');
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'template');
+
+define('APP_NAME', 'KalimaFramework');
+define('APP_SLOGAN', 'Framework MVC php');
+define('APP_COMPANY', 'www.webcol.net');
+
+#webcol seguridad
+define('KEY_MD5', 'P0L1');
+
+
+#base de datos
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'kalima');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHAR', 'utf8');
