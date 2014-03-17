280c6903e0d8e5d1cffd84718b7c92b69ff62c9d
Efrasoft <webcol@users.noreply.github.com>
2014-03-17 10:23:04 -0500

    Update Km_Config.php

--- system/core/Km_Config.php
+++ system/core/Km_Config.php
@@ -1,6 +1,6 @@
 <?php
 
-
+#Configuracion Basica
 define('BASE_URL', '');
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_LAYOUT', 'template');
@@ -14,6 +14,7 @@ define('KEY_MD5', 'P0L1');
 
 
 #base de datos
+// Configuracion de tu base de datos
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'kalima');
 define('DB_USER', 'root');
