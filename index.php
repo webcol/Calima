0a61c76b1cdb69e27ba81652222670bb0930e37c
Efrasoft <webcol@users.noreply.github.com>
2014-03-19 22:29:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,10 @@ define('IMG_PATH', ROOT . 'public_'.DS.'img' . DS);
 define('JS_PATH', ROOT . 'public_'.DS.'js' . DS);
 define('APP_LIBS', ROOT . 'system'.DS.'libs' . DS);
 
+
+
+require_once APP_PATH . 'Km_Autocarga.php';
 require_once APP_PATH . 'Km_Config.php';
-require_once APP_PATH . 'Km_Solicitud.php';
-require_once APP_PATH . 'Km_Bootstrap.php';
-require_once APP_PATH . 'Km_Controller.php';
-require_once APP_PATH . 'Km_Model.php';
-require_once APP_PATH . 'Km_View.php';
-require_once APP_PATH . 'Km_Registro.php';
-require_once APP_PATH . 'Km_Database.php';
 
 try{
     Km_Bootstrap::run(new Km_Solicitud);
