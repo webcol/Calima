ce9e05feca6fc4a8cb8c282e3e0fdd04d59cfb95
Efrasoft <webmaster@webcol.net>
2014-05-22 21:52:34 -0500

    cambios a las vistas

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS);
 define('APP_PATH', ROOT . 'sistema'.DS.'nucleo' . DS);
 # define para mvc erp
-define('VIEW_PATH', ROOT . 'mvc'.DS.'views' . DS);
+define('VIEW_PATH', ROOT . 'mvc'.DS.'vistas' . DS);
 define('CSS_PATH', ROOT . 'public_'.DS.'css' . DS);
 define('IMG_PATH', ROOT . 'public_'.DS.'img' . DS);
 define('JS_PATH', ROOT . 'public_'.DS.'js' . DS);
--- sistema/nucleo/Cf_Vista.php
+++ sistema/nucleo/Cf_Vista.php
@@ -14,18 +14,18 @@ class Cf_Vista
         
         
         $_layoutParams = array(
-            'ruta_css' => BASE_URL . 'mvc/views/layout/' . DEFAULT_LAYOUT . '/css/',
-            'ruta_img' => BASE_URL . 'mvc/views/layout/' . DEFAULT_LAYOUT . '/img/',
-            'ruta_js' => BASE_URL . 'mvc/views/layout/' . DEFAULT_LAYOUT . '/js/'
+            'ruta_css' => BASE_URL . 'mvc/vistas/layout/' . DEFAULT_LAYOUT . '/css/',
+            'ruta_img' => BASE_URL . 'mvc/vistas/layout/' . DEFAULT_LAYOUT . '/img/',
+            'ruta_js' => BASE_URL . 'mvc/vistas/layout/' . DEFAULT_LAYOUT . '/js/'
         );
         
         $rutaView = VIEW_PATH . $this->_controlador . DS . $vista . '.phtml';
         
         if(is_readable($rutaView)){
-            include_once ROOT . 'mvc/views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'header.php';
+            include_once ROOT . 'mvc/vistas'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'header.php';
             include_once $rutaView;
             //include_once ROOT . 'mvc/views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'menu.php';
-            include_once ROOT . 'mvc/views'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'footer.php';
+            include_once ROOT . 'mvc/vistas'. DS . 'layout' . DS . DEFAULT_LAYOUT . DS . 'footer.php';
         } 
         else {
             throw new Exception('vista no encontrada');
