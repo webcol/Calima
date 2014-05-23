cc89377303843c75870abc4cdaf0eecfec34ae30
Efrasoft <webmaster@webcol.net>
2014-05-22 21:22:18 -0500

    nombre de directorio

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS);
-define('APP_PATH', ROOT . 'system'.DS.'core' . DS);
+define('APP_PATH', ROOT . 'sistema'.DS.'core' . DS);
 # define para mvc erp
 define('VIEW_PATH', ROOT . 'mvc'.DS.'views' . DS);
 define('CSS_PATH', ROOT . 'public_'.DS.'css' . DS);
 define('IMG_PATH', ROOT . 'public_'.DS.'img' . DS);
 define('JS_PATH', ROOT . 'public_'.DS.'js' . DS);
-define('APP_LIBS', ROOT . 'system'.DS.'libs' . DS);
+define('APP_LIBS', ROOT . 'sistema'.DS.'libs' . DS);
 
 
 
