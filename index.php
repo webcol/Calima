f34680ce1a013e9dd317f231cbd3ae4275c2eb1a
Efrasoft <webmaster@webcol.net>
2014-06-24 19:32:21 -0500

    cambios a index

--- index.php
+++ index.php
@@ -30,37 +30,3 @@ try{
 catch(Exception $e){
     echo $e->getMessage();
 }
-
-
-
-/*
- * <?php
-
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', realpath(dirname(__FILE__)) . DS);
-define('APP_PATH', ROOT . 'sistema'.DS.'nucleo' . DS);
-# define para mvc erp
-define('VIEW_PATH', ROOT . 'mvc'.DS.'vistas' . DS);
-define('CONTR_PATH', ROOT . 'mvc'.DS.'controladores' . DS);
-define('CSS_PATH', ROOT . 'public_'.DS.'css' . DS);
-define('IMG_PATH', ROOT . 'public_'.DS.'img' . DS);
-define('JS_PATH', ROOT . 'public_'.DS.'js' . DS);
-define('APP_LIBS', ROOT . 'sistema'.DS.'librerias' . DS);
-
-
-//echo ROOT;
-require_once APP_PATH . 'Cf_Autocarga.php';
-require_once APP_PATH . 'Cf_Configuracion.php';
-
-/*require_once APP_PATH . 'Cf_Bootstrap.php';
-require_once APP_PATH . 'Cf_Solicitud.php';*/
-
-//Cf_Bootstrap::run(new Cf_Solicitud);
-/*try{
-  Cf_Bootstrap::run(new Cf_Solicitud);
-}
-catch(Exception $e){
-    echo $e->getMessage();
-}
-
- */
