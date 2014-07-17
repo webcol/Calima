11bcc56987e5281acdbc890a5379880d2a5d7d03
Efrasoft <webcol@users.noreply.github.com>
2014-07-16 23:41:29 -0500

    creando un objeto a la clase basededatos

--- sistema/nucleo/Cf_Modelo.php
+++ sistema/nucleo/Cf_Modelo.php
@@ -3,22 +3,11 @@
 //namespace sistema\nucleo;
 //use sistema\nucleo as nucleo;
 
-abstract class Cf_Modelo
+class Cf_Modelo
 {
-    protected $_vista;
+    protected $_bd;
     
     public function __construct() {
-        $this->_vista = new Cf_Vista(new Cf_Solicitud);
-    }
-    
-    protected function cargaLibModelo($orm){
-        $rutaLib = RUTA_ORM . $orm . '.php';
-        if(is_readable($rutaLib)){
-            require_once $rutaLib;
-           //echo 'libreria cargada';
-        }
-        else {
-            throw new Exception("Error al cargar libreria");
-        }
+        $this->_bd=new Cf_Basedatos();
     }
 }
