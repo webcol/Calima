1b102cebf217315284318d0503070d3b97fd8584
Efrasoft <webmaster@webcol.net>
2014-07-18 11:52:26 -0500

    se desarrolla una clase ayudante

---
+++ sistema/ayudantes/PHPAyuda.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class PHPAyuda {
+    
+    //seguridad
+    
+    public function filtrarTexto($texto){
+        return strip_tags($texto);
+    }
+    
+    public function filtrarCaracteresEspeciales($texto){
+        return htmlspecialchars($texto, ENT_QUOTES);
+    }
+}
