711906aec6e4a11a064d9795db333937608e51f1
Efrasoft <webmaster@webcol.net>
2014-07-19 11:52:45 -0500

    cargamos la clase ayudantes para usar sus metodos de ayuda en el envio de email

--- mvc/controladores/contactosController.php
+++ mvc/controladores/contactosController.php
@@ -4,11 +4,17 @@ class contactosController extends Cf_Controlador
 {
     public function __construct() {
         parent::__construct();
+        
+        // cargamos la clase ayudantes para usar sus metodos de ayuda
+        $this->cargaAyudante('PHPAyuda');
+        $this->_ayud= new PHPAyuda;
     }
     
     public function index()
     {
         $this->_vista->titulo = 'CalimaFramework';
         $this->_vista->renderizar('index', 'inicio');
+        
+        
     }
 }
