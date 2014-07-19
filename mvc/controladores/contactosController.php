3e30022b57a36d723b2dd332ce98b9fe4495d27f
Efrasoft <webmaster@webcol.net>
2014-07-19 12:45:35 -0500

    se modifica contactos

--- mvc/controladores/contactosController.php
+++ mvc/controladores/contactosController.php
@@ -13,7 +13,7 @@ class contactosController extends Cf_Controlador
     public function index()
     {
         $this->_vista->titulo = 'CalimaFramework';
-        $this->_vista->renderizar('index', 'inicio');
+        $this->_vista->imprimirVista('index', 'inicio');
         
         
     }
