d7137ef2a78c6c5334d1db699a16d3b1a53605d5
Efrasoft <webmaster@webcol.net>
2014-07-19 12:51:54 -0500

    creamos directorio para guardar guia de uso en pdf y html

---
+++ mvc/vistas/layout/adicionales/footer.php
@@ -0,0 +1,21 @@
+ <!-- Site footer -->
+        <div id="footer">
+ 
+            <center> Copyleft &copy; 2013 <?php echo APP_EMPRESA; ?></center>
+            
+       </div>
+      
+
+    <!-- JavaScript -->
+    <script src="<?php echo $_layoutParams['ruta_js']; ?>jquery-1.10.2.js"></script>
+    <script src="<?php echo $_layoutParams['ruta_js']; ?>bootstrap.js"></script>
+
+    <!-- Page Specific Plugins -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+    <script src="<?php echo $_layoutParams['ruta_js']; ?>morris/chart-data-morris.js"></script>
+    <script src="<?php echo $_layoutParams['ruta_js']; ?>tablesorter/jquery.tablesorter.js"></script>
+    <script src="<?php echo $_layoutParams['ruta_js']; ?>tablesorter/tables.js"></script>
+
+  </body>
+</html>
