c352616c2e2041f080b3bfce3728f95c9dc9c83e
webcol <webcol@webcol-PC>
2014-08-26 22:07:12 -0500

    Cambios en configuracion CF

--- sistema/nucleo/Cf_Configuracion.php
+++ sistema/nucleo/Cf_Configuracion.php
@@ -1,5 +1,9 @@
 <?php
 
+
+
+/* More information on this file at http://www.calimaframework.com/info/ */
+
 #Configuracion Basica
 define('BASE_URL', '');
 define('DEFAULT_CONTROLLER', 'index');
@@ -10,14 +14,40 @@ define('APP_SLOGAN', 'Framework MVC php');
 define('APP_EMPRESA', 'www.webcol.net');
 
 #webcol seguridad
-define('KEY_MD5', 'P0L1');
+define('CF_KEY_MD5', 'P0L1');
 
 
 #base de datos
 // Configuracion de tu base de datos
-define('BD_HOST', 'localhost');
-define('BD_NOMBRE', 'calima');
-define('BD_USUARIO', 'efrasoft');
-define('BD_CLAVE', '654321aa');
-define('BD_CHAR', 'utf8');
-define('BD_CONECTOR', 'mysql');
+define('CF_BD_HOST', 'localhost');
+define('CF_BD_NOMBRE', 'calima');
+define('CF_BD_USUARIO', 'juank');
+define('CF_BD_CLAVE', '654321aa');
+define('CF_BD_CHAR', 'utf8');
+define('CF_BD_CONECTOR', 'mysql');
+
+/* Defino zona horaria*/
+date_default_timezone_set('America/Bogota');
+
+/* Set default date format */
+define('Cf_FORMATO_FECHA', 'l, d F Y');
+
+/* CalimaFramework license key */
+define('Cf_LICENSE', 'Cf-O17N-JHK8-TDJL-B5AO-8WKA'); //'FREE-5RTY-POI8-0UYT-IUYT-TGH6'
+
+/* Enable or disable warning messages */
+define('CF_DEBUG', TRUE);
+
+/* Default application locale */
+define('CF_LOCALE', 'es_ES');
+
+/* Translatable locale format */
+define('CF_LOCALE_DATE_FORMAT', '%A, %d %B %G');
+
+/* Translatable time format */
+define('CF_LOCALE_TIME_FORMAT', ' %T');
+
+/* Default location to store application data. Must be protected from public. */
+define('CF_PATH_DATA', dirname(__FILE__) . '/bb-data');
+
+ini_set('memory_limit', '256M');
