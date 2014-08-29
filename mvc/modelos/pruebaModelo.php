3d177714c85615a8d5bdb0333418a91f018a26b4
webcol <webcol@webcol-PC>
2014-08-29 17:04:26 -0500

    Nuevos cambios en pruebaModelo referente a insertar datos en tabla

--- mvc/modelos/pruebaModelo.php
+++ mvc/modelos/pruebaModelo.php
@@ -13,4 +13,14 @@ class pruebaModelo extends Cf_Modelo {
         
        
     }
+    
+    public function insertarDatos(){
+        
+        $post=$this->_bd->consulta('INSERT INTO post (id, titulo, cuerpo) VALUES (:id, :titulo, :cuerpo)');
+        $post=$this->_bd->enlace(':id', '4');
+        $post=$this->_bd->enlace(':titulo', 'efrasoft');
+        $post=$this->_bd->enlace(':cuerpo', 'este es el cuerpo 23');
+        $post=$this->_bd->ejecucion();
+        //return $post=$this->_bd->resultset();
+    }
 }
