b81de6c9d48ee8005f932156ca7ae72c92baf20b
webcol <webcol@webcol-PC>
2014-11-10 22:32:30 -0500

    Nuevos cambios en el footer

--- mvc/vistas/adicionales/footer.php
+++ mvc/vistas/adicionales/footer.php
@@ -7,15 +7,6 @@
       
 
     <!-- JavaScript -->
-    <script src="<?php echo $_layoutParams['ruta_js']; ?>jquery-1.10.2.js"></script>
-    <script src="<?php echo $_layoutParams['ruta_js']; ?>bootstrap.js"></script>
-
-    <!-- Page Specific Plugins -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
-    <script src="<?php echo $_layoutParams['ruta_js']; ?>morris/chart-data-morris.js"></script>
-    <script src="<?php echo $_layoutParams['ruta_js']; ?>tablesorter/jquery.tablesorter.js"></script>
-    <script src="<?php echo $_layoutParams['ruta_js']; ?>tablesorter/tables.js"></script>
-
+    
   </body>
 </html>
