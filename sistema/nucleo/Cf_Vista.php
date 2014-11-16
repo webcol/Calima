61498d33a42ecff542ec1b0768b2be2a7c604484
webcol <webcol@webcol-PC>
2014-11-16 08:16:55 -0500

    Se realiza cambios en variables para el Cf_vista

--- sistema/nucleo/Cf_Vista.php
+++ sistema/nucleo/Cf_Vista.php
@@ -12,12 +12,12 @@ class Cf_Vista
     public function imprimirVista($vista, $item = false)
     {            
             
-        $rutaView = VIEW_PATH . $this->_controlador . DS . $vista . '.phtml';
+        $ver_ruta = VIEW_PATH . $this->_controlador . DS . $vista . '.phtml';
         
-        if(is_readable($rutaView)){
-            include_once SITE_ROOT . 'mvc/vistas' . DS . ADICIONALES_VISTA . DS . 'header.php';
-            include_once $rutaView;
-            include_once SITE_ROOT . 'mvc/vistas' . DS . ADICIONALES_VISTA . DS . 'footer.php';
+        if(is_readable($ver_ruta)){
+            include_once SITE_ROOT . 'mvc/vistas' . DS . ADICIONALES_VISTA . DS . 'encabezado.php';
+            include_once $ver_ruta;
+            include_once SITE_ROOT . 'mvc/vistas' . DS . ADICIONALES_VISTA . DS . 'pie_de_pagina.php';
         } 
         else {
             throw new Exception('houston tenemos un problema! vista no encontrada');
