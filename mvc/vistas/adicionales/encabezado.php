b8fa5af2427cb3648b40aa4abb7b590493352245
webcol <webcol@webcol-PC>
2014-11-16 11:03:28 -0500

    Cambios en el encabezado

--- mvc/vistas/adicionales/encabezado.php
+++ mvc/vistas/adicionales/encabezado.php
@@ -8,12 +8,6 @@
 
     <title><?php if(isset($this->titulo)) echo $this->titulo; ?></title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="<?php echo $_layoutParams['ruta_css']; ?>bootstrap.css" rel="stylesheet" type="text/css" />
-
-    <!-- Add custom CSS here -->
-    <link href="<?php echo $_layoutParams['ruta_css']; ?>justified-nav.css" rel="stylesheet">
-    <!-- Page Specific CSS -->
-    <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
+    
     
   </head>
