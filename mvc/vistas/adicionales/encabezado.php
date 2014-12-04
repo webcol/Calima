71ecfbd13576426fe825f19210dba2dcad8469aa
webcol <webcol@webcol-PC>
2014-12-04 08:02:05 -0500

    Cambios en las vistas

--- mvc/vistas/adicionales/encabezado.php
+++ mvc/vistas/adicionales/encabezado.php
@@ -6,6 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     
+        
     <!-- Google Analytics -->
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
