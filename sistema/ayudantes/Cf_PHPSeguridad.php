9aecb77c405051261e2c0e3a2ed2139e32ecbaeb
webcol <webcol@webcol-PC>
2014-12-06 22:40:23 -0500

    Se crean nuevas funciones de seguridad

--- sistema/ayudantes/Cf_PHPSeguridad.php
+++ sistema/ayudantes/Cf_PHPSeguridad.php
@@ -11,6 +11,14 @@ class Cf_PHPSeguridad {
 
 //Seguridad
     
+    //xss funciones de mitigacion
+    function xsseguro($texto,$encoding='UTF-8'){
+       return htmlspecialchars($texto,ENT_QUOTES | ENT_HTML401,$encoding);
+    }
+    function xecho($texto){
+       echo xsseguro($texto);
+    }
+    
     public function filtrarTexto($texto){
         return strip_tags($texto);
     }
@@ -21,4 +29,29 @@ class Cf_PHPSeguridad {
         return htmlspecialchars($texto, ENT_QUOTES);
     }
     
+    
+    // Genracion de Cadenas Aleatorias
+    function generarCadenaAleatoria($longitud = 10) {
+        $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $longitudCaracteres = strlen($caracteres);
+        $cadenaAleatoria = '';
+        for ($i = 0; $i < $length; $i++) {
+             $cadenaAleatoria .= $caracteres[rand(0, $longitudCaracteres - 1)];
+        }
+        return $cadenaAleatoria;
+    }
+    // Protecion CSRF
+    public function generoTokenDeFormulario($formulario) {
+        $secreta =  Cf_CSRF_SECRET.$this->generarCadenaAleatoria();
+        $sid = session_id();
+        $token = md5($secreta.$sid.$formulario);
+        return $token;
+    }
+    
+    public function verificoTokenDeFormulario($formulario, $token) {
+        $secreta =  Cf_CSRF_SECRET.$this->generarCadenaAleatoria();
+        $sid = session_id();
+        $correcta = md5($secreta.$sid.$formulario);
+        return ($token == $correcta);
+    }
 }
