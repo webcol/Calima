12312c154e28b01850890e3b36190a286ef7cd93
webcol <webcol@webcol-PC>
2015-01-20 21:19:35 -0500

    Actualizacion del controlador Index

--- mvc/controladores/indexControlador.php
+++ mvc/controladores/indexControlador.php
@@ -28,21 +28,7 @@ class indexControlador extends Cf_Controlador
         $this->_vista->titulo = 'CalimaFramework';
         $this->_vista->imprimirVista('index', 'inicio');
         $this->_sesion->iniciarSesion('_s', false);
-    }
-    
-    public function datox(){
-        echo "daoxxxxx";
-    }
-    
-    public function pruebas()
-    {
-        $datas = $this->cargaMod('prueba');
-        $datas->
-        $this->_vista->postear= $datas->llamarDatos();
-        
-        $this->_vista->titulo = 'CalimaFramework';
-        $this->_vista->imprimirVista('index', 'inicio');
-    }
+    }  
     
     public function filtro($texto){
         
@@ -57,39 +43,35 @@ class indexControlador extends Cf_Controlador
         
         error_reporting(0);
         // Establecer propiedades
-$this->_exc->getProperties()
-->setCreator("Cattivo")
-->setLastModifiedBy("Cattivo")
-->setTitle("Documento Excel de Prueba")
-->setSubject("Documento Excel de Prueba")
-->setDescription("Demostracion sobre como crear archivos de Excel desde PHP.")
-->setKeywords("Excel Office 2007 openxml php")
-->setCategory("Pruebas de Excel");
+        $this->_exc->getProperties()
+        ->setCreator("Cattivo")
+        ->setLastModifiedBy("Cattivo")
+        ->setTitle("Documento Excel de Prueba")
+        ->setSubject("Documento Excel de Prueba")
+        ->setDescription("Demostracion sobre como crear archivos de Excel desde PHP.")
+        ->setKeywords("Excel Office 2007 openxml php")
+        ->setCategory("Pruebas de Excel");
 
 // Agregar Informacion
-$this->_exc->setActiveSheetIndex(0)
-->setCellValue('A1', 'Valor 1')
-->setCellValue('B1', 'Valor 2')
-->setCellValue('C1', 'Total')
-->setCellValue('A2', '10')
-->setCellValue('C2', '=sum(A2:B2)');
+        $this->_exc->setActiveSheetIndex(0)
+        ->setCellValue('A1', 'Valor 1')
+        ->setCellValue('B1', 'Valor 2')
+        ->setCellValue('C1', 'Total')
+        ->setCellValue('A2', '10')
+        ->setCellValue('C2', '=sum(A2:B2)');
 
 // Renombrar Hoja
-$this->_exc->getActiveSheet()->setTitle('Tecnologia Simple');
+        $this->_exc->getActiveSheet()->setTitle('Tecnologia Simple');
 
 // Establecer la hoja activa, para que cuando se abra el documento se muestre primero.
-$this->_exc->setActiveSheetIndex(0);
+        $this->_exc->setActiveSheetIndex(0);
 
 // Se modifican los encabezados del HTTP para indicar que se envia un archivo de Excel.
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="pruebaReal.xlsx"');
-header('Cache-Control: max-age=0');
-$objWriter = PHPExcel_IOFactory::createWriter($_exc, 'Excel2007');
-$objWriter->save('php://output');
-exit;
-
-        
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="pruebaReal.xlsx"');
+        header('Cache-Control: max-age=0');
+        $objWriter = PHPExcel_IOFactory::createWriter($_exc, 'Excel2007');
+        $objWriter->save('php://output');
+        exit;
     }
-    
-    
 }
