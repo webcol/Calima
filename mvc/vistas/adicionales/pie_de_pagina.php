795287d4304aa8860b89020165f0d1c73fab9fa1
webcol <webcol@webcol-PC>
2015-01-20 20:28:32 -0500

    Actulaizacion archivo pie de pagina con constante de creditos

--- mvc/vistas/adicionales/pie_de_pagina.php
+++ mvc/vistas/adicionales/pie_de_pagina.php
@@ -1,7 +1,7 @@
  <!-- Site footer -->
         <div id="footer">
  
-            <center> Copyleft  2013 <?php echo CF_AP_EMPRESA; ?></center>
+            <center><?php echo CF_AP_CREDITOS; ?></center>
             
        </div>
       
