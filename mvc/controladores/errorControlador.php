e47577b7b6d7f067f75d0bd36dcf27655db05648
webcol <webcol@webcol-PC>
2015-02-21 17:34:05 -0500

    Se agrega controlador error para impresion de errores

---
+++ mvc/controladores/errorControlador.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class errorControlador extends Cf_Controlador{
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function index(){
+        
+        $this->_vista->titulo="Error detectado";
+        $this->_vista->imprimirVista('index','error');
+    }
+    
+}
