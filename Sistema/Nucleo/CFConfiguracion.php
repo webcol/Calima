bcce07ed7340611c7c7cb43b87d6d09b9d927836
webcol <webcol@webcol-PC>
2016-03-16 23:42:56 -0500

    Cambios en el archivo CFConfiguracion.php

--- Sistema/Nucleo/CFConfiguracion.php
+++ Sistema/Nucleo/CFConfiguracion.php
@@ -24,77 +24,22 @@
  */
 
 
-/* inicio la configuracion*/
-define('Cf_CONFIG_INICIO', 'false');
-
-/* Defino zona horaria*/
-date_default_timezone_set('America/Bogota');
-
-/* Define una cuenta de correo para uso del app */
-define('DESTINO_EMAIL', 'info@calimaframework.com');
-
-/* Defino el formato de fecha */
-define('Cf_FORMATO_FECHA', 'l, d F Y');
-
-/* CalimaFramework clave de licencia */
-define('Cf_LICENSE', 'Cf-O17N-JHK8-TDJL-B5AO-8WKA');
-
-/* mensajes de warnin */
-define('CF_DEBUG', TRUE);
-
-/* Idioma local */
-define('CF_LOCALE', 'es_ES');
-
-/* Create una cuenta con google analytics y agrega el UA en la constante */
-define('CF_ANALYTICS', 'UA-xxxxxx');
-
-/* con la siguiente constante podras crear una ip fija de tu empresa para hacer 
-* pruebas en tu entorno de red basado en tu ip que te ofrece tu proveedor de servicio
-*/
-define('Cf_IPPRUEBAS', 'x.x.x.x');
-
-/* Transladamos a formato local */
-define('CF_FECHA_FORMATO_LOCAL', '%A, %d %B %G');
-
-/* Translatable time format */
-define('CF_LOCALE_TIME_FORMAT', ' %T');
-
-/* Por defecto almacenamos los datos de la aplicacion. */
-define('CF_PATH_DATA', dirname(__FILE__) . '/Cf-data');
-
-#Configuracion Basica
-
 /* La siguiente CONSTANTE permite el apuntapiento para archivos js, css, imagenes desde la vista hacia el directorio _public */
-define('Cf_BASE_URL', 'http://localhost/Calima/');
+// esta constante es muy importante para el inicio de Cf Usted debe de escribr aqui el url de su sitio
+// Ejemplo: http://misitioweb.co/ o http://localhost/midirectorio/
+define('Cf_BASE_URL', 'http://localhost/calima/');
 
 /* definimos un controlador inicial en nuestro proyecto */
 define('CONTROLADOR_INICIAL', 'instalador');
 
+
 /* Sedefine una CONSTANTE al directorio adicionales en la vista */
 define('ADICIONALES_VISTA', 'adicionales');
 
-/* Definimos una CONSTANTE como nombre de aplicacion */
-define('CF_AP_NOMBRE', 'CalimaFramework');
-
-/* Definimos un Slogan para la aplicacion web */
-define('CF_AP_SLOGAN', 'Tu Framework php MVC hispano ');
-
-/* Empresa de la aplicacion */
-define('CF_AP_EMPRESA', 'www.webcol.net');
 
 /* Creditos de la aplicacion */
-define('CF_AP_CREDITOS', 'Licencia MIT 2014 - 2016 Debeloped by www.webcol.net');
+define('CF_AP_CREDITOS', 'CopyLeft 2015 Debeloped by www.webcol.net');
 
-#webcol seguridad
-
-/* Definimos un indice de clave para concatenar en encriptacion de datos cambie por su seguridad */
-define('Cf_KEY_MD5', 'P0L1');
-
-/*  en el controlador concatena la constante con el llamado a la funcion generarCadenaAleatoria() de Cf_PHPSeguridad Cambie por seguridad*/
-define('Cf_CSRF_SECRET','Cfbeta');
-
-/*Si usted va a utilizar SSL debe de cambiar a true */
-define('Cf_SESION_PARAMETRO_SEGURO','false');
 
 /* #base de datos */
 
