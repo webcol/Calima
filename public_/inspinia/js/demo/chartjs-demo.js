2cd6ea69cad1afa88adf40bc19cdea3afc866405
WebcolGroup <webmaster@webcol.net>
2018-04-28 17:27:33 -0500

    Se actualiza la plantilla bootstrap

--- public_/inspinia/js/demo/chartjs-demo.js
+++ public_/inspinia/js/demo/chartjs-demo.js
@@ -3,165 +3,101 @@ $(function () {
     var lineData = {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [
+
             {
-                label: "Example dataset",
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "Example dataset",
-                fillColor: "rgba(26,179,148,0.5)",
-                strokeColor: "rgba(26,179,148,0.7)",
-                pointColor: "rgba(26,179,148,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(26,179,148,1)",
+                label: "Data 1",
+                backgroundColor: 'rgba(26,179,148,0.5)',
+                borderColor: "rgba(26,179,148,0.7)",
+                pointBackgroundColor: "rgba(26,179,148,1)",
+                pointBorderColor: "#fff",
                 data: [28, 48, 40, 19, 86, 27, 90]
+            },{
+                label: "Data 2",
+                backgroundColor: 'rgba(220, 220, 220, 0.5)',
+                pointBorderColor: "#fff",
+                data: [65, 59, 80, 81, 56, 55, 40]
             }
         ]
     };
 
     var lineOptions = {
-        scaleShowGridLines: true,
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-        scaleGridLineWidth: 1,
-        bezierCurve: true,
-        bezierCurveTension: 0.4,
-        pointDot: true,
-        pointDotRadius: 4,
-        pointDotStrokeWidth: 1,
-        pointHitDetectionRadius: 20,
-        datasetStroke: true,
-        datasetStrokeWidth: 2,
-        datasetFill: true,
-        responsive: true,
+        responsive: true
     };
 
 
     var ctx = document.getElementById("lineChart").getContext("2d");
-    var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
+    new Chart(ctx, {type: 'line', data: lineData, options:lineOptions});
 
     var barData = {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [
             {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,0.8)",
-                highlightFill: "rgba(220,220,220,0.75)",
-                highlightStroke: "rgba(220,220,220,1)",
+                label: "Data 1",
+                backgroundColor: 'rgba(220, 220, 220, 0.5)',
+                pointBorderColor: "#fff",
                 data: [65, 59, 80, 81, 56, 55, 40]
             },
             {
-                label: "My Second dataset",
-                fillColor: "rgba(26,179,148,0.5)",
-                strokeColor: "rgba(26,179,148,0.8)",
-                highlightFill: "rgba(26,179,148,0.75)",
-                highlightStroke: "rgba(26,179,148,1)",
+                label: "Data 2",
+                backgroundColor: 'rgba(26,179,148,0.5)',
+                borderColor: "rgba(26,179,148,0.7)",
+                pointBackgroundColor: "rgba(26,179,148,1)",
+                pointBorderColor: "#fff",
                 data: [28, 48, 40, 19, 86, 27, 90]
             }
         ]
     };
 
     var barOptions = {
-        scaleBeginAtZero: true,
-        scaleShowGridLines: true,
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-        scaleGridLineWidth: 1,
-        barShowStroke: true,
-        barStrokeWidth: 2,
-        barValueSpacing: 5,
-        barDatasetSpacing: 1,
-        responsive: true,
-    }
+        responsive: true
+    };
 
 
-    var ctx = document.getElementById("barChart").getContext("2d");
-    var myNewChart = new Chart(ctx).Bar(barData, barOptions);
+    var ctx2 = document.getElementById("barChart").getContext("2d");
+    new Chart(ctx2, {type: 'bar', data: barData, options:barOptions});
 
-    var polarData = [
-        {
-            value: 300,
-            color: "#a3e1d4",
-            highlight: "#1ab394",
-            label: "App"
-        },
-        {
-            value: 140,
-            color: "#dedede",
-            highlight: "#1ab394",
-            label: "Software"
-        },
-        {
-            value: 200,
-            color: "#b5b8cf",
-            highlight: "#1ab394",
-            label: "Laptop"
-        }
-    ];
+    var polarData = {
+        datasets: [{
+            data: [
+                300,140,200
+            ],
+            backgroundColor: [
+                "#a3e1d4", "#dedede", "#b5b8cf"
+            ],
+            label: [
+                "My Radar chart"
+            ]
+        }],
+        labels: [
+            "App","Software","Laptop"
+        ]
+    };
 
     var polarOptions = {
-        scaleShowLabelBackdrop: true,
-        scaleBackdropColor: "rgba(255,255,255,0.75)",
-        scaleBeginAtZero: true,
-        scaleBackdropPaddingY: 1,
-        scaleBackdropPaddingX: 1,
-        scaleShowLine: true,
-        segmentShowStroke: true,
-        segmentStrokeColor: "#fff",
         segmentStrokeWidth: 2,
-        animationSteps: 100,
-        animationEasing: "easeOutBounce",
-        animateRotate: true,
-        animateScale: false,
-        responsive: true,
+        responsive: true
 
     };
 
-    var ctx = document.getElementById("polarChart").getContext("2d");
-    var myNewChart = new Chart(ctx).PolarArea(polarData, polarOptions);
+    var ctx3 = document.getElementById("polarChart").getContext("2d");
+    new Chart(ctx3, {type: 'polarArea', data: polarData, options:polarOptions});
 
-    var doughnutData = [
-        {
-            value: 300,
-            color: "#a3e1d4",
-            highlight: "#1ab394",
-            label: "App"
-        },
-        {
-            value: 50,
-            color: "#dedede",
-            highlight: "#1ab394",
-            label: "Software"
-        },
-        {
-            value: 100,
-            color: "#b5b8cf",
-            highlight: "#1ab394",
-            label: "Laptop"
-        }
-    ];
+    var doughnutData = {
+        labels: ["App","Software","Laptop" ],
+        datasets: [{
+            data: [300,50,100],
+            backgroundColor: ["#a3e1d4","#dedede","#b5b8cf"]
+        }]
+    } ;
+
 
     var doughnutOptions = {
-        segmentShowStroke: true,
-        segmentStrokeColor: "#fff",
-        segmentStrokeWidth: 2,
-        percentageInnerCutout: 45, // This is 0 for Pie charts
-        animationSteps: 100,
-        animationEasing: "easeOutBounce",
-        animateRotate: true,
-        animateScale: false,
-        responsive: true,
+        responsive: true
     };
 
 
-    var ctx = document.getElementById("doughnutChart").getContext("2d");
-    var myNewChart = new Chart(ctx).Doughnut(doughnutData, doughnutOptions);
+    var ctx4 = document.getElementById("doughnutChart").getContext("2d");
+    new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
 
 
     var radarData = {
@@ -169,49 +105,24 @@ $(function () {
         datasets: [
             {
                 label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
+                backgroundColor: "rgba(220,220,220,0.2)",
+                borderColor: "rgba(220,220,220,1)",
                 data: [65, 59, 90, 81, 56, 55, 40]
             },
             {
                 label: "My Second dataset",
-                fillColor: "rgba(26,179,148,0.2)",
-                strokeColor: "rgba(26,179,148,1)",
-                pointColor: "rgba(26,179,148,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
+                backgroundColor: "rgba(26,179,148,0.2)",
+                borderColor: "rgba(26,179,148,1)",
                 data: [28, 48, 40, 19, 96, 27, 100]
             }
         ]
     };
 
     var radarOptions = {
-        scaleShowLine: true,
-        angleShowLineOut: true,
-        scaleShowLabels: false,
-        scaleBeginAtZero: true,
-        angleLineColor: "rgba(0,0,0,.1)",
-        angleLineWidth: 1,
-        pointLabelFontFamily: "'Arial'",
-        pointLabelFontStyle: "normal",
-        pointLabelFontSize: 10,
-        pointLabelFontColor: "#666",
-        pointDot: true,
-        pointDotRadius: 3,
-        pointDotStrokeWidth: 1,
-        pointHitDetectionRadius: 20,
-        datasetStroke: true,
-        datasetStrokeWidth: 2,
-        datasetFill: true,
-        responsive: true,
-    }
+        responsive: true
+    };
 
-    var ctx = document.getElementById("radarChart").getContext("2d");
-    var myNewChart = new Chart(ctx).Radar(radarData, radarOptions);
+    var ctx5 = document.getElementById("radarChart").getContext("2d");
+    new Chart(ctx5, {type: 'radar', data: radarData, options:radarOptions});
 
 });
--- public_/inspinia/js/inspinia.js
+++ public_/inspinia/js/inspinia.js
@@ -1,14 +1,28 @@
-// Custom scripts
+/*
+ *
+ *   INSPINIA - Responsive Admin Theme
+ *   version 2.7.1
+ *
+ */
+
 $(document).ready(function () {
 
-    // MetsiMenu
+
+    // Add body-small class if window less than 768px
+    if ($(this).width() < 769) {
+        $('body').addClass('body-small')
+    } else {
+        $('body').removeClass('body-small')
+    }
+
+    // MetisMenu
     $('#side-menu').metisMenu();
- 
+
     // Collapse ibox function
-    $('.collapse-link:not(.binded)').addClass("binded").click( function() {
+    $('.collapse-link').on('click', function () {
         var ibox = $(this).closest('div.ibox');
         var button = $(this).find('i');
-        var content = ibox.find('div.ibox-content');
+        var content = ibox.children('.ibox-content');
         content.slideToggle(200);
         button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
         ibox.toggleClass('').toggleClass('border-bottom');
@@ -17,123 +31,256 @@ $(document).ready(function () {
             ibox.find('[id^=map-]').resize();
         }, 50);
     });
- 
+
     // Close ibox function
-    $('.close-link:not(.binded)').addClass("binded").click( function() {
+    $('.close-link').on('click', function () {
         var content = $(this).closest('div.ibox');
         content.remove();
     });
- 
+
+    // Fullscreen ibox function
+    $('.fullscreen-link').on('click', function () {
+        var ibox = $(this).closest('div.ibox');
+        var button = $(this).find('i');
+        $('body').toggleClass('fullscreen-ibox-mode');
+        button.toggleClass('fa-expand').toggleClass('fa-compress');
+        ibox.toggleClass('fullscreen');
+        setTimeout(function () {
+            $(window).trigger('resize');
+        }, 100);
+    });
+
+    // Close menu in canvas mode
+    $('.close-canvas-menu').on('click', function () {
+        $("body").toggleClass("mini-navbar");
+        SmoothlyMenu();
+    });
+
+    // Run menu of canvas
+    $('body.canvas-menu .sidebar-collapse').slimScroll({
+        height: '100%',
+        railOpacity: 0.9
+    });
+
+    // Open close right sidebar
+    $('.right-sidebar-toggle').on('click', function () {
+        $('#right-sidebar').toggleClass('sidebar-open');
+    });
+
+    // Initialize slimscroll for right sidebar
+    $('.sidebar-container').slimScroll({
+        height: '100%',
+        railOpacity: 0.4,
+        wheelStep: 10
+    });
+
+    // Open close small chat
+    $('.open-small-chat').on('click', function () {
+        $(this).children().toggleClass('fa-comments').toggleClass('fa-remove');
+        $('.small-chat-box').toggleClass('active');
+    });
+
+    // Initialize slimscroll for small chat
+    $('.small-chat-box .content').slimScroll({
+        height: '234px',
+        railOpacity: 0.4
+    });
+
     // Small todo handler
-    $('.check-link:not(.binded)').addClass("binded").click( function(){
+    $('.check-link').on('click', function () {
         var button = $(this).find('i');
         var label = $(this).next('span');
         button.toggleClass('fa-check-square').toggleClass('fa-square-o');
         label.toggleClass('todo-completed');
         return false;
     });
- 
+
     // Append config box / Only for demo purpose
-    /*
-    $.get("skin-config.html", function (data) {
-        $('body').append(data);
-    });
-    */
- 
-    // minimalize menu
-    $('.navbar-minimalize:not(.binded)').addClass("binded").click(function () {
+    // Uncomment on server mode to enable XHR calls
+    //$.get("skin-config.html", function (data) {
+    //    if (!$('body').hasClass('no-skin-config'))
+    //        $('body').append(data);
+    //});
+
+    // Minimalize menu
+    $('.navbar-minimalize').on('click', function (event) {
+        event.preventDefault();
         $("body").toggleClass("mini-navbar");
         SmoothlyMenu();
-    })
- 
-    // tooltips
+
+    });
+
+    // Tooltips demo
     $('.tooltip-demo').tooltip({
         selector: "[data-toggle=tooltip]",
         container: "body"
-    })
- 
-    // Move modal to body
-    // Fix Bootstrap backdrop issu with animation.css
-    $('.modal').appendTo("body")
- 
+    });
+
+
     // Full height of sidebar
     function fix_height() {
         var heightWithoutNavbar = $("body > #wrapper").height() - 61;
-        $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
+        $(".sidebar-panel").css("min-height", heightWithoutNavbar + "px");
+
+        var navbarheight = $('nav.navbar-default').height();
+        var wrapperHeight = $('#page-wrapper').height();
+
+        if (navbarheight > wrapperHeight) {
+            $('#page-wrapper').css("min-height", navbarheight + "px");
+        }
+
+        if (navbarheight < wrapperHeight) {
+            $('#page-wrapper').css("min-height", $(window).height() + "px");
+        }
+
+        if ($('body').hasClass('fixed-nav')) {
+            if (navbarheight > wrapperHeight) {
+                $('#page-wrapper').css("min-height", navbarheight + "px");
+            } else {
+                $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
+            }
+        }
+
     }
+
     fix_height();
- 
+
     // Fixed Sidebar
-    // unComment this only whe you have a fixed-sidebar
-            //    $(window).bind("load", function() {
-            //        if($("body").hasClass('fixed-sidebar')) {
-            //            $('.sidebar-collapse').slimScroll({
-            //                height: '100%',
-            //                railOpacity: 0.9,
-            //            });
-            //        }
-            //    })
- 
-    $(window).bind("load resize click scroll", function() {
-        if(!$("body").hasClass('body-small')) {
+    $(window).bind("load", function () {
+        if ($("body").hasClass('fixed-sidebar')) {
+            $('.sidebar-collapse').slimScroll({
+                height: '100%',
+                railOpacity: 0.9
+            });
+        }
+    });
+
+    // Move right sidebar top after scroll
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav')) {
+            $('#right-sidebar').addClass('sidebar-top');
+        } else {
+            $('#right-sidebar').removeClass('sidebar-top');
+        }
+    });
+
+    $(window).bind("load resize scroll", function () {
+        if (!$("body").hasClass('body-small')) {
             fix_height();
         }
-    })
- 
+    });
+
     $("[data-toggle=popover]")
         .popover();
+
+    // Add slimscroll to element
+    $('.full-height-scroll').slimscroll({
+        height: '100%'
+    })
 });
 
 
+// Minimalize menu when screen is less than 768px
+$(window).bind("resize", function () {
+    if ($(this).width() < 769) {
+        $('body').addClass('body-small')
+    } else {
+        $('body').removeClass('body-small')
+    }
+});
+
+// Local Storage functions
+// Set proper body class and plugins based on user configuration
+$(document).ready(function () {
+    if (localStorageSupport()) {
+
+        var collapse = localStorage.getItem("collapse_menu");
+        var fixedsidebar = localStorage.getItem("fixedsidebar");
+        var fixednavbar = localStorage.getItem("fixednavbar");
+        var boxedlayout = localStorage.getItem("boxedlayout");
+        var fixedfooter = localStorage.getItem("fixedfooter");
+
+        var body = $('body');
+
+        if (fixedsidebar == 'on') {
+            body.addClass('fixed-sidebar');
+            $('.sidebar-collapse').slimScroll({
+                height: '100%',
+                railOpacity: 0.9
+            });
+        }
+
+        if (collapse == 'on') {
+            if (body.hasClass('fixed-sidebar')) {
+                if (!body.hasClass('body-small')) {
+                    body.addClass('mini-navbar');
+                }
+            } else {
+                if (!body.hasClass('body-small')) {
+                    body.addClass('mini-navbar');
+                }
+
+            }
+        }
+
+        if (fixednavbar == 'on') {
+            $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
+            body.addClass('fixed-nav');
+        }
+
+        if (boxedlayout == 'on') {
+            body.addClass('boxed-layout');
+        }
+
+        if (fixedfooter == 'on') {
+            $(".footer").addClass('fixed');
+        }
+    }
+});
+
+// check if browser support HTML5 local storage
+function localStorageSupport() {
+    return (('localStorage' in window) && window['localStorage'] !== null)
+}
+
 // For demo purpose - animation css script
-window.animationHover = function(element, animation){
+function animationHover(element, animation) {
     element = $(element);
     element.hover(
-        function() {
+        function () {
             element.addClass('animated ' + animation);
         },
-        function(){
+        function () {
             //wait for animation to finish before removing classes
-            window.setTimeout( function(){
+            window.setTimeout(function () {
                 element.removeClass('animated ' + animation);
             }, 2000);
         });
-};
+}
 
-// Minimalize menu when screen is less than 768px
-$(function() {
-    $(window).bind("load resize", function() {
-        if ($(this).width() < 769) {
-            $('body').addClass('body-small')
-        } else {
-            $('body').removeClass('body-small')
-        }
-    })
-})
-
-window.SmoothlyMenu = function() {
+function SmoothlyMenu() {
     if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
         // Hide menu in order to smoothly turn on when maximize menu
         $('#side-menu').hide();
         // For smoothly turn on menu
         setTimeout(
             function () {
-                $('#side-menu').fadeIn(500);
-            }, 100);
-    } else if ($('body').hasClass('fixed-sidebar')){
+                $('#side-menu').fadeIn(400);
+            }, 200);
+    } else if ($('body').hasClass('fixed-sidebar')) {
         $('#side-menu').hide();
         setTimeout(
             function () {
-                $('#side-menu').fadeIn(500);
-            }, 300);
+                $('#side-menu').fadeIn(400);
+            }, 100);
     } else {
         // Remove all inline style from jquery fadeIn function to reset menu state
         $('#side-menu').removeAttr('style');
     }
-};
+}
 
 // Dragable panels
-window.WinMove = function() {
+function WinMove() {
     var element = "[class*=col]";
     var handle = ".ibox-title";
     var connect = "[class*=col]";
@@ -143,9 +290,9 @@ window.WinMove = function() {
             connectWith: connect,
             tolerance: 'pointer',
             forcePlaceholderSize: true,
-            opacity: 0.8,
+            opacity: 0.8
         })
         .disableSelection();
-};
+}
 
 
